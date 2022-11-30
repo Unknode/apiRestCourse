44f0e250a0bfe3799353376ea554c79733a365fc
Unknode <thezalthor@hotmail.com>
2022-11-30 00:24:32 -0300

    Continue adding user and token JWT

--- ApiLessons/ApiRest/Repository/UserRepository.cs
+++ ApiLessons/ApiRest/Repository/UserRepository.cs
@@ -2,6 +2,10 @@
 using ApiRest.Model;
 using ApiRest.Model.Context;
 using ApiRest.Repository.Token;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ApiRest.Repository
 {
@@ -16,8 +20,20 @@ namespace ApiRest.Repository
 
         public AuthUser ValidateUser(AuthUserVO authUser)
         {
+            var password = ComputeHash(authUser.Password, new SHA256CryptoServiceProvider());
 
-            return null;
+            return _context.Users.FirstOrDefault(u => (u.UserName == authUser.UserName) && (u.Password == password));
+        }
+
+        private string ComputeHash(string password, SHA256CryptoServiceProvider hash)
+        {
+            if (password == null || hash == null)
+                return String.Empty;
+
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+            Byte[] hashedBytes = hash.ComputeHash(inputBytes);
+
+            return BitConverter.ToString(hashedBytes);
         }
     }
 }
